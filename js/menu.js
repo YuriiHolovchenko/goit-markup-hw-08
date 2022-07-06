32599a0f354d251c500a18be0d3401282cea71aa
Yurii Holovchenko <yougo2034@gmail.com>
2022-07-06 21:10:45 +0300

    add footer

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,7 @@
 (() => {
     const menuBtnRef = document.querySelector("[data-menu-button]");
     const mobileMenuRef = document.querySelector("[data-menu]");
+    // const logo = document.getElementsByClassName('logo');
     
     menuBtnRef.addEventListener("click", () => {
         const expanded =
@@ -10,6 +11,8 @@
         menuBtnRef.setAttribute("aria-expanded", !expanded);
 
         mobileMenuRef.classList.toggle("is-open");
+
+        // logo.classList.toggle("is-hidden");
 
     });
 
